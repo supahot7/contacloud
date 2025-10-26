de4c574cb81bc72c9956ab340c0d2a699fdcacb5
gabymcanales <gabycanales728@gmail.com>
2025-10-26 15:14:47 -0600

    nuevas pantallas, (nueva transaccion y panel de estados financieros)

--- contabilidad/urls.py
+++ contabilidad/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.login_view, name='login'),  # raíz de la app
     path('login/', views.login_view, name='login'),
     path('panel/', views.panel_principal, name='panel_principal_extra'),  # opcional, si quieres acceso directo
-    path('catalogo/', views.catalogo_cuentas, name='catalogoCuentas'),  # catálogo de cuentas
+    path('catalogo/', views.catalogo_cuentas, name='catalogo_cuentas'),  # catálogo de cuentas
+    path('transacciones/', views.nueva_transaccion, name='nueva_transaccion'), # nueva transaccion
+    path('estados/', views.estado_financiero, name='estado_financiero'),  # Estados financieros
     
 ]
--- contabilidad/views.py
+++ contabilidad/views.py
@@ -26,10 +26,23 @@ def panel_principal(request):
 # -------------------------
 def catalogo_cuentas(request):
     # Se recomienda usar snake_case en el nombre del template
-    return render(request, 'contabilidad/catalogoCuentas.html')
+    return render(request, 'contabilidad/catalogo_cuentas.html')
+
+# -------------------------
+# Nueva Transacción
+# -------------------------
+def nueva_transaccion(request):
+    # Puedes pasar datos al template si es necesario, por ejemplo para activar el item del menú
+    context = {
+        'active_page': 'transacciones' # Esto ayuda a destacar "Transacciones" en el sidebar
+    }
+    return render(request, 'contabilidad/nueva_transaccion.html', context)
 
 
 
-
-
-
+# -------------------------
+# Estado Financiero
+# -------------------------
+def estado_financiero(request):
+    # Renderiza el template de estados financieros
+    return render(request, 'contabilidad/estado_financiero.html')
