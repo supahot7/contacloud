ffa2873ad39068a247a4f98371cf5c920cb6af70
YamiLinares <ll22030@ues.edu.sv>
2025-10-27 13:30:41 -0600

    Prototipos

--- contabilidad/urls.py
+++ contabilidad/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('panel/', views.panel_principal, name='panel_principal_extra'),  # opcional, si quieres acceso directo
     path('catalogo/', views.catalogo_cuentas, name='catalogoCuentas'),  # catálogo de cuentas
-    
+    path('estados/', views.estados_financieros, name='estadosFinancieros'), # Estados Financieros
+    path('balance/', views.balance_general, name='balanceGeneral'), # Balance General
+    path('estado/', views.estado_resultados, name='estadoResultados'), # Estado de Resultados
+    path('estadoCapital/', views.estado_capital, name='estadoCapital'), # Estado de Capital
+    path('contabilidadCostos/', views.contabilidad_costos, name='contabilidadCostos'), # Contabilidad de Costos
+    path('manoObra/', views.mano_obra, name='manoObra'), # Mano de obra
+    path('inventario/', views.inventario_licencias, name='inventarioLicencias'), # Inventario de Licencias
 ]
--- contabilidad/views.py
+++ contabilidad/views.py
@@ -28,8 +28,53 @@ def catalogo_cuentas(request):
     # Se recomienda usar snake_case en el nombre del template
     return render(request, 'contabilidad/catalogoCuentas.html')
 
+# -------------------------
+# Estados Financieros
+# -------------------------
 
+def estados_financieros(request):
+    return render(request, 'contabilidad/estadosFinancieros.html')
 
+# -------------------------
+# Balance General
+# -------------------------
 
+def balance_general(request):
+    return render(request, 'contabilidad/balanceGeneral.html')
+
+# -------------------------
+# Estado de Resultados
+# -------------------------
+
+def estado_resultados(request):
+    return render(request, 'contabilidad/estadoResultados.html')
+
+# -------------------------
+# Estado de Capital
+# -------------------------
+
+def estado_capital(request):
+    return render(request, 'contabilidad/estadoCapital.html')
+
+# -------------------------
+# Estado de Capital
+# -------------------------
+
+def contabilidad_costos(request):
+    return render(request, 'contabilidad/contabilidadCostos.html')
+
+# -------------------------
+# Mano de Obra
+# -------------------------
+
+def mano_obra(request):
+    return render(request, 'contabilidad/manoObra.html')
+
+# -------------------------
+# Inventario de Licencias
+# -------------------------
+
+def inventario_licencias(request):
+    return render(request, 'contabilidad/inventarioLicencias.html')
 
 
